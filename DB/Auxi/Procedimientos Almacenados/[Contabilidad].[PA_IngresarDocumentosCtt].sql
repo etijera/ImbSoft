9075a9440cd2d148212281805d31c253f1d23c30
etijera <36046939+etijera@users.noreply.github.com>
2018-02-02 14:28:14 -0500

    Cambios BD

--- DB/Auxi/Procedimientos Almacenados/[Contabilidad].[PA_IngresarDocumentosCtt].sql
+++ DB/Auxi/Procedimientos Almacenados/[Contabilidad].[PA_IngresarDocumentosCtt].sql
@@ -4,56 +4,57 @@ IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[Contabilida
 GO
 
 CREATE PROCEDURE [Contabilidad].[PA_IngresarDocumentosCtt]  
-		@Operacion			VARCHAR(12)		=NULL
-		,@Consecutivo		VARCHAR(12)		= NULL
-		,@Fuente			VARCHAR(4)		= NULL
+		@Operacion			VARCHAR(12)		= NULL
+		,@Consecutivo		VARCHAR(24)		= NULL
+		,@Fuente			VARCHAR(6)		= NULL
 		,@Fecha				DATETIME		= NULL
-		,@NotaAclaratoria	VARCHAR(MAX)  = NULL		
+		,@NotaAclaratoria	VARCHAR(MAX)	= NULL		
 AS
 BEGIN
 	IF @Operacion = 'SD'
 	BEGIN
-		SELECT  accglmas.Mascod				Codigo	
-				,ISNULL(accglpuc.glnum,'')	Cuenta 					
-				,ISNULL(accGLMAS.masdet,'')	Detalle
-				,ISNULL(procencos.ccocod,'')Ccosto	
-				,ISNULL(procencos.cconom,'')DesCcosto
-				,ISNULL(accglTer.ternit,'')	Tercero 
-				,ISNULL(accglTer.ternom,'')	NomTercero 
-				,accglmas.masdeb			Debe 
-				,0							Haber 
-				,accglpuc.glcct				RCcosto
-				,accglmas.MasOT				OT
-				,ordenes.ordnom             DetOT
-		FROM	accGLMAS
-				LEFT JOIN accglter		ON accglTer.tercod   = accGLMAS.Master
-				LEFT JOIN procencos		ON procencos.ccocod	 = accglmas.Mascco
-				LEFT JOIN accglpuc		ON accglPUC.glcod	 = accGLMAS.Mascod
-				LEFT JOIN ordenes       ON ordenes.ordnro    = accGLMAS.MasOT
-		WHERE	accGLMAS.Masfte= @Fuente 
-				AND accGLMAS.Masdoc=  @Consecutivo   AND Masfec = @Fecha
-				AND accGLMAS.Masdeb>0 			
+		SELECT  MA.Cuenta							Codigo	
+				,ISNULL(PUC.Nombre,'')				Cuenta 					
+				,ISNULL(MA.Descripcion,'')			Detalle
+				,ISNULL(MA.CodCentroCosto,'')		Ccosto	
+				,ISNULL(CC.Nombre,'')				DesCcosto
+				,ISNULL(TE.DocumentoIdentidad,'')	Tercero 
+				,ISNULL(TE.RazonSocial,'')			NomTercero 
+				,MA.ValorDebe						Debe 
+				,0									Haber 
+				,PUC.Ccosto							RCcosto
+				,''									CTT
+				,''									DetCTT
+		FROM	Contabilidad.CabeceraAsientos AS CA
+				INNER JOIN Contabilidad.MovimientosAsiento AS MA ON MA.Asiento = CA.Id
+				LEFT JOIN Terceros AS TE ON TE.Codigo = MA.CodTercero 
+				LEFT JOIN Contabilidad.CentroCostos AS CC ON CC.Codigo = MA.CodCentroCosto
+				LEFT JOIN Contabilidad.Puc AS PUC ON PUC.Codigo = MA.Cuenta
+				--LEFT JOIN ordenes       ON ordenes.ordnro    = accGLMAS.MasOT Debe ser la tabla de contrato
+		WHERE	CA.CodigoFuente = @Fuente 
+				AND CA.Comprobante =  @Consecutivo AND CA.Fecha = @Fecha
+				AND MA.ValorDebe > 0 			
 		UNION ALL
-		SELECT	accglmas.Mascod				Codigo	
-				,ISNULL(accglpuc.glnum,'')	Cuenta 					
-				,ISNULL(accGLMAS.masdet,'')	Detalle
-				,ISNULL(procencos.ccocod,'')Ccosto	
-				,ISNULL(procencos.cconom,'')DesCcosto
-				,ISNULL(accglTer.ternit,'')	Tercero 
-				,ISNULL(accglTer.ternom,'')	NomTercero 
-				,0							Debe 
-				,accglmas.Mashbr			Haber 
-				,accglpuc.glcct				RCcosto
-				,accglmas.MasOT				OT
-				,ordenes.ordnom             DetOT
-		FROM	accGLMAS 
-				LEFT JOIN accglter		ON accglTer.tercod   = accGLMAS.Master
-				LEFT JOIN procencos		ON procencos.ccocod	 = accglmas.Mascco
-				LEFT JOIN accglpuc		ON accglPUC.glcod	 = accGLMAS.Mascod
-				LEFT JOIN ordenes       ON ordenes.ordnro    = accGLMAS.MasOT
-		WHERE	accGLMAS.Masfte=  @Fuente 
-				AND accGLMAS.Masdoc=  @Consecutivo 
-				AND accGLMAS.Mashbr>0   AND Masfec = @Fecha		
-		ORDER BY accglmas.Mascod
+		SELECT  MA.Cuenta							Codigo	
+				,ISNULL(PUC.Nombre,'')				Cuenta 					
+				,ISNULL(MA.Descripcion,'')			Detalle
+				,ISNULL(MA.CodCentroCosto,'')		Ccosto	
+				,ISNULL(CC.Nombre,'')				DesCcosto
+				,ISNULL(TE.DocumentoIdentidad,'')	Tercero 
+				,ISNULL(TE.RazonSocial,'')			NomTercero 
+				,0									Debe 
+				,MA.ValorHaber						Haber 
+				,PUC.Ccosto							RCcosto
+				,''									CTT
+				,''									DetCTT
+		FROM	Contabilidad.CabeceraAsientos AS CA
+				INNER JOIN Contabilidad.MovimientosAsiento AS MA ON MA.Asiento = CA.Id
+				LEFT JOIN Terceros AS TE ON TE.Codigo = MA.CodTercero 
+				LEFT JOIN Contabilidad.CentroCostos AS CC ON CC.Codigo = MA.CodCentroCosto
+				LEFT JOIN Contabilidad.Puc AS PUC ON PUC.Codigo = MA.Cuenta
+				--LEFT JOIN ordenes       ON ordenes.ordnro    = accGLMAS.MasOT Debe ser la tabla de contrato
+		WHERE	CA.CodigoFuente = @Fuente 
+				AND CA.Comprobante =  @Consecutivo AND CA.Fecha = @Fecha
+				AND MA.ValorDebe > 0 	
 	END	
 END
